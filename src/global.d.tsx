d7976bdf584042667d7a53b8e4636177f066949d
unknown <withbbang@gmail.com>
2022-06-02 16:36:46 +0900

    이미지 타입 글로벌 파일로 이동

--- src/global.d.tsx
+++ src/global.d.tsx
@@ -1,3 +1,7 @@
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.svg";
 declare module "*.scss" {
   const content: { [className: string]: string };
   export = content;
