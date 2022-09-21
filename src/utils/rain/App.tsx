0828e8a9b5d088ba98988028fbd90ea5c732e92c
Bread-Kim <withbbang@gmail.com>
2022-09-21 19:17:14 +0900

    로컬 컴퓨터 변경으로 인한 테스트 커밋

--- src/utils/rain/App.tsx
+++ src/utils/rain/App.tsx
@@ -11,7 +11,7 @@ class App {
   constructor() {
     this.canvas = document.createElement("canvas");
     this.canvas.style.background = "#000000";
-    this.count = 1000;
+    this.count = 500;
     const rainTag = document.getElementById("rain");
     rainTag && rainTag.appendChild(this.canvas);
 
