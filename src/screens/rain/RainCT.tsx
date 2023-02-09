11588564c5df53b55aa051c514a72cc085d00129
unknown <withbbang@gmail.com>
2023-02-09 13:34:23 +0900

    add bgm

--- src/screens/rain/RainCT.tsx
+++ src/screens/rain/RainCT.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import RainPT from "./RainPT";
 import App from "utils/rain/App";
+import bgm from "assets/mp3/rain_sound.MP3";
 
 const RainCT = () => {
   useEffect(() => {
     new App();
   }, []);
 
-  return <RainPT />;
+  return <RainPT bgm={bgm} />;
 };
 
 export default RainCT;
--- src/screens/rain/RainPT.tsx
+++ src/screens/rain/RainPT.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const RainPT = () => (
-  <div id="rain" style={{ width: "100%", height: "100%" }}></div>
+const RainPT = (props: any) => (
+  <>
+    <div id="rain" style={{ width: "100%", height: "100%" }}></div>
+    <audio autoPlay={true} loop>
+      <source src={props.bgm} type="audio/mp3"></source>
+    </audio>
+  </>
 );
 
 export default RainPT;
